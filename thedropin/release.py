3f25ffc80ed1e5541faccc26b2d5532d487fea48
Tom <tom@blackforestbotanics.com>
2019-10-07 08:10:54 -0700

    deploy on tagging test

--- thedropin/release.py
+++ thedropin/release.py
@@ -51,10 +51,8 @@ enter new version: '''
 
     os.system('git add -A')
     os.system(f'git commit -m "release v{new_version}"')
-    os.system('git push')
-
     os.system(f'git tag {new_version}')
-    os.system('git push --tags')
+    os.system('git push --follow-tags')
 
     print('tags pushed.\n')
 
